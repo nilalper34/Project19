4302118e658ad295935487316a16f7f2fd5c3de9
CodeDebugRun <c7105erhan@gmail.com>
2022-10-18 15:04:32 +0300

    erhanPage

--- src/test/java/pages/Erhanpgc.java
+++ src/test/java/pages/Erhanpgc.java
@@ -1,4 +1,10 @@
 package pages;
 
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class Erhanpgc {
+    public Erhanpgc() {
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
 }
