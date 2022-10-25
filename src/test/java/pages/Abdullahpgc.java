852ad7c4a4a18f3758edc6704684f9eef0090c92
Abdullah AVLIK <abdullah.avlik@gmail.com>
2022-10-26 00:40:54 +0300

    abdullah_22.10.26/00:40

--- src/test/java/pages/Abdullahpgc.java
+++ src/test/java/pages/Abdullahpgc.java
@@ -1,4 +1,133 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+import java.util.List;
+
 public class Abdullahpgc {
+    public Abdullahpgc() {
+        PageFactory.initElements(Driver.getDriver(), this);
+
+    }
+
+    @FindBy(xpath = "//span[text()='Sign In']")
+    public WebElement SignIn;
+    @FindBy(xpath = "//input[@id='username']")
+    public WebElement username;
+    @FindBy(xpath = "//input[@id='password']")
+    public WebElement password;
+    @FindBy(xpath = "//button[text()='Sign In']")
+    public WebElement signInButton;
+    @FindBy(xpath = "//a[text()='My Account']")
+    public WebElement myAccount;
+
+    @FindBy(xpath = "//a[text()='Store Manager']")
+    public WebElement storeManager;
+
+    @FindBy(xpath = "(//span[@class='wcfmfa fa-cube'])[1]")
+    public WebElement products;
+
+    @FindBy(xpath = "//tbody/tr/td[2]")
+    public List<WebElement> productsList;
+
+    @FindBy(xpath = "//div[text()='Inventory']")
+    public WebElement inventory;
+
+    @FindBy(xpath = "//strong[text()='Manage Stock?']")
+    public WebElement manageStock;
+
+    @FindBy(xpath = "//input[@id='manage_stock']")
+    public WebElement manageStockClickBox;
+    @FindBy(xpath = "//input[@id='stock_qty']")
+    public WebElement stockQtyInput;
+
+    @FindBy(xpath = "//strong[text()='Stock Qty']")
+    public WebElement stockQty;
+
+    @FindBy(xpath = "//input[@id='wcfm_products_simple_submit_button' and @class='wcfm_submit_button']")
+    public WebElement submitButton;
+
+    @FindBy(xpath = "(//*[contains(text(),'Product Successfully Published.')])[1]")
+    public WebElement successButton;
+
+    @FindBy(xpath = "//*[@class='wcfm-message wcfm-success']")
+    public WebElement successButton1;
+
+    @FindBy(xpath = "//strong[.='Allow Backorders?'] ")
+    public WebElement allowBackOrders;
+
+    @FindBy(xpath = "//select[@id='backorders']")
+    public WebElement dropDownBackOrders;
+
+    //"(//div[@class='page_collapsible_content_holder'])[4]"
+    @FindBy(xpath = "//div[text()='Shipping']")
+    public WebElement shipping;
+
+    @FindBy(xpath = "//p[@class='weight wcfm_title']")
+    public WebElement weight;
+
+    @FindBy(xpath = "//p[@class='length wcfm_title']")
+    public WebElement dimensions;
+    @FindBy(xpath = "//input[@id='weight']")
+    public WebElement weightInput;
+    @FindBy(xpath = "//input[@id='length']")
+    public WebElement lengthInput;
+    @FindBy(xpath = "//input[@id='width']")
+    public WebElement widthInput;
+    @FindBy(xpath = "//input[@id='height'] ")
+    public WebElement heightInput;
+
+    @FindBy(css = "p[class='_wcfmmp_processing_time wcfm_title']")
+    public WebElement processingTime;
+
+    @FindBy(css = "#_wcfmmp_processing_time")
+    public WebElement processingTimeDropDown;
+
+    @FindBy(xpath = "(//div[@class='page_collapsible_content_holder'])[5]")
+    public WebElement attributes;
+
+    @FindBy(xpath = "//strong[.='Color']")
+    public WebElement color;
+
+    @FindBy(xpath = "//strong[.='Size']")
+    public WebElement size;
+
+    @FindBy(id = "attributes_is_active_1")
+    public WebElement colorCheckBox;
+
+    @FindBy(xpath = "(//button[text()='Select all'])[1]")
+    public WebElement colorSelectAll;
+
+    @FindBy(id = "attributes_is_active_2")
+    public WebElement sizeCheckBox;
+
+    @FindBy(xpath = "(//button[text()='Select all'])[2]")
+    public WebElement sizeSelectAll;
+
+    @FindBy(xpath = "(//ul)[25]/li")
+    public List<WebElement> colorList;
+
+
+    @FindBy(xpath = "(//ul)[26]/li")
+    public List<WebElement> sizeList;
+
+    @FindBy(id = "wcfm_products_manage_form_toptan-urun-gosterme-ayarlari_head")
+    public WebElement toptanUGA;
+    @FindBy(xpath = "//p[@class='piecetype wcfm_title field_61d331d8dc5a9']")
+    public WebElement pieceType;
+    @FindBy(xpath = "//p[@class='unitpercart wcfm_title field_61d33933d88bd']")
+    public WebElement unitPerPiece;
+    @FindBy(xpath = "//p[@class='minorderqtytr wcfm_title field_61d33a93da83d']")
+    public WebElement minOrderQuantity;
+    @FindBy(xpath = "//select[@id='piecetype']")
+    public WebElement dropDownPieceType;
+    @FindBy(xpath = "//input[@id='unitpercart']")
+    public WebElement inputUnitPerPiece;
+     @FindBy(xpath = "//input[@id='minorderqtytr']")
+    public WebElement inputMinOrderQuantity;
+
+
 }
---
+++ src/test/java/tests/US_11/TC_002_WithMethods.java
@@ -0,0 +1,54 @@
+package tests.US_11;
+
+import com.github.javafaker.Faker;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import pages.Abdullahpgc;
+import utilities.AbdullahReusableMethods;
+import utilities.Driver;
+import utilities.TestBaseRapor;
+
+import java.time.Duration;
+import java.util.Random;
+
+public class TC_002_WithMethods extends TestBaseRapor {
+    public Abdullahpgc hub = new Abdullahpgc();
+    public Actions actions = new Actions(Driver.getDriver());
+    public SoftAssert softAssert = new SoftAssert();
+    public Random random = new Random();
+    public JavascriptExecutor jse = (JavascriptExecutor) Driver.getDriver();
+
+    public WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(15));
+    public Faker faker = new Faker();
+
+    @Test
+    public void testCase002() throws InterruptedException {
+        extentTest=extentReports.createTest("Pozitif Test","Units per piece");
+        AbdullahReusableMethods.loginProductsPage();
+        //Açilan sayfada aşağı kısımda bulunan menülerden  "Toptan Ürün Gösterme Ayarları" butonuna tıklanır
+        Thread.sleep(2000);
+        actions.sendKeys(Keys.PAGE_DOWN).
+                sendKeys(Keys.PAGE_DOWN).
+                sendKeys(Keys.PAGE_DOWN).perform();
+        hub.toptanUGA.click();
+        extentTest.info("Açilan sayfada aşağı kısımda bulunan menülerden  \"Toptan Ürün Gösterme Ayarları\" butonuna tıklandı");
+        //Açılan sayfada "Units Per Piece" yazısının görünür olduğu test edilir
+        wait .until(ExpectedConditions.visibilityOf(hub.unitPerPiece));
+        Assert.assertTrue(hub.unitPerPiece.isDisplayed());
+        extentTest.info("Açılan sayfada \"Units Per Piece\" yazısının görünür olduğu test edildi");
+        //"Units Per Piece" alanının erişilebilir olduğu test edilir ve geçerli bir sayı girilir
+        wait.until(ExpectedConditions.visibilityOf(hub.inputUnitPerPiece));
+        hub.inputUnitPerPiece.isEnabled();
+        hub.inputUnitPerPiece.clear();
+        hub.inputUnitPerPiece.sendKeys(""+random.nextInt(100));
+        extentTest.info("\"Units Per Piece\" alanının erişilebilir olduğu test edilir ve geçerli bir sayı girildi");
+
+        AbdullahReusableMethods.validation();
+    }
+}
